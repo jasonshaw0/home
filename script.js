559ec24705515c4e4064b38afb936f03169cae44
Jason Shaw <35120806+jasonshaw0@users.noreply.github.com>
2024-03-05 17:15:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
-function toggleDropdown(dropdownId) {
-  var dropdown = document.getElementById(dropdownId);
-  dropdown.classList.toggle("show");
-
-  var otherDropdownId = dropdownId === "myDropdown" ? "skillsDropdown" : "myDropdown";
-  var otherDropdown = document.getElementById(otherDropdownId);
-  if (otherDropdown.classList.contains('show') && dropdown.classList.contains('show')) {
-      otherDropdown.classList.remove('show');
-  }
-}
-
-// Close the dropdowns if the user clicks outside
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-          }
-      }
-  }
-}
-
 function animateProgressBar(progressBar, targetWidth) {
   let width = 0;
   const progressBarElement = progressBar.querySelector(".progress");
 
   const interval = setInterval(() => {
-      if (width >= targetWidth) {
-          clearInterval(interval);
-      } else {
-          width++;
-          progressBarElement.style.width = width + "%";
-      }
-  }, 30); // Adjust the interval for smoother animation
+    if (width >= targetWidth) {
+      clearInterval(interval);
+    } else {
+      width++;
+      progressBarElement.style.width = width + "%";
+    }
+  }, 10); // Adjust the interval for smoother animation
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const progressBars = document.querySelectorAll(".progress-bar");
-  const targetWidths = [90, 85, 80, 70]; // Adjust the target widths accordingly
+  const targetWidths = [90, 90, 85, 70]; // Adjust the target widths accordingly
 
   progressBars.forEach((progressBar, index) => {
-      animateProgressBar(progressBar, targetWidths[index]);
+    animateProgressBar(progressBar, targetWidths[index]);
   });
 });
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+}
+
+function toggleDropdown() {
+      var dropdownContent = document.getElementById("dropdownContent");
+      var arrowIcon = document.getElementById("arrowIcon");
+
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+        arrowIcon.classList.remove("rotate-up");
+      } else {
+        dropdownContent.style.display = "block";
+        arrowIcon.classList.add("rotate-up");
+      }
+    }
