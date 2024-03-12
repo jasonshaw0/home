180148ca4294f5ffa066c29b722373fd09024104
Jason Shaw <35120806+jasonshaw0@users.noreply.github.com>
2024-03-12 14:29:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ function toggleDropdown() {
         arrowIcon.classList.add("rotate-up");
       }
     }
+
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+      var i = 0;
+      var txt = 'Lorem ipsum dummy text blabla.';
+      var speed = 50;
+      
+    }
